138490e338fe5109b284dfee15ee90908bac4413
dazhabin <dazhabin@gmail.com>
2023-06-15 17:44:55 -0400

    add translator functions

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,5 +2,10 @@ from deep_translator import MyMemoryTranslator
 
 def englishToFrench(englishText):
     #write the code here
-    frenchText = ""
+    frenchText = MyMemoryTranslator(source='english', target='french').translate(englishText)
     return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    englishText = MyMemoryTranslator(source='french', target='english').translate(frenchText)
+    return englishText
