f4ef04c25457449d6e612dea0b06d66458a452de
dazhabin <dazhabin@gmail.com>
2023-06-16 05:49:46 -0400

    add unit tests

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,11 +1,19 @@
+"""
+Functions for English to French and French to English translation
+"""
+
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    #write the code here
-    frenchText = MyMemoryTranslator(source='english', target='french').translate(englishText)
-    return frenchText
+def english_to_french(english_text):
+    """
+    This function returns string translated from English to French
+    """
+    french_text = MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    #write the code here
-    englishText = MyMemoryTranslator(source='french', target='english').translate(frenchText)
-    return englishText
+def french_to_english(french_text):
+    """
+    This function returns string translated from French to English
+    """
+    english_text = MyMemoryTranslator(source='french', target='english').translate(french_text)
+    return english_text
